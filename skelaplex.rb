20d1944b2f92dc707e64f59b7ee6458cb4a1f9e8
Jef Amirault <admiraljef@gmail.com>
2016-03-26 11:49:22 -0400

    opens chrome and rubymine when necessary. updated to use phusion passenger

--- skelaplex.rb
+++ skelaplex.rb
@@ -12,7 +12,7 @@ end
 start
 
 tmux 'new-session -d -s skelaplex -n Server'
-tmux "send-keys -t skelaplex:0 \"cd #{APP_DIR}; rails s -p #{PORT}\" C-m"
+tmux "send-keys -t skelaplex:0 \"cd #{APP_DIR}; bundle exec passenger start -p #{PORT}\" C-m"
 
 tmux 'split-window -v -t skelaplex:0'
 tmux 'resize-pane -D -t skelaplex:0 9'
@@ -22,6 +22,21 @@ tmux 'send-keys -t skelaplex:0 "rails c" C-m'
 
 tmux 'new-window -t skelaplex:1 -n Bash'
 tmux "send-keys -t skelaplex:1 \"cd #{APP_DIR}; git show --stat\" C-m"
+
+need_chrome = `pgrep chrome`.empty?
+tmux 'new-session -d -s Browser -n Chrome'
+tmux "send-keys -t Browser:0 \"google-chrome 127.0.0.1:#{PORT}\" C-m"
+unless need_chrome
+  tmux 'send-keys -t Browser:0 \"exit\" C-m'
+end
+
+if `pgrep rubymine`.empty?
+
+  rubymine_path = '/opt/RubyMine-8.0.3/bin/rubymine.sh'
+
+  tmux 'new-session -d -s IDE -n Rubymine'
+  tmux "send-keys -t IDE:0 \"#{rubymine_path}\" C-m"
+end
 
 tmux 'select-window -t skelaplex:0'
 tmux 'attach-session -t skelaplex'
