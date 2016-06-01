951a319124435315622ea587830c718c52ca1b12
Jeffrey Amirault <jeffreyamirault@gmail.com>
2016-06-01 12:09:28 -0400

    daemonize IDE instead of running in separate tmux session

--- skelaplex.rb
+++ skelaplex.rb
@@ -5,9 +5,9 @@ IDE_PATH = '/opt/RubyMine-2016.1.1/bin/rubymine.sh'
 postgres = false
 
 start_server_code = if postgres
-	"bundle exec passenger start -p #{PORT}"
+  "bundle exec passenger start -p #{PORT}"
 else
-	"rails s -p #{PORT}"
+  "rails s -p #{PORT}"
 end
 
 
@@ -37,14 +37,17 @@ start
 tmux 'new-session -d -s skelaplex -n Server'
 tmux "send-keys -t skelaplex:0 \"cd #{APP_DIR}; #{start_server_code}\" C-m"
 
-tmux 'split-window -v -t skelaplex:0'
-tmux 'resize-pane -D -t skelaplex:0 9'
-tmux "send-keys -t skelaplex:0 \"cd #{APP_DIR}\" C-m C-l"
-tmux 'send-keys -t skelaplex:0 "rails c" C-m'
+# tmux 'split-window -v -t skelaplex:0'
+# tmux 'resize-pane -D -t skelaplex:0 9'
+# tmux "send-keys -t skelaplex:0 \"cd #{APP_DIR}\" C-m C-l"
+# tmux 'send-keys -t skelaplex:0 "rails c" C-m'
 
 
-tmux 'new-window -t skelaplex:1 -n Bash'
-tmux "send-keys -t skelaplex:1 \"cd #{APP_DIR}; git show --stat\" C-m"
+tmux 'new-window -t skelaplex:1 -n RSpec'
+tmux "send-keys -t skelaplex:1 \"cd #{APP_DIR}; guard\" C-m"
+
+tmux 'new-window -t skelaplex:2 -n Bash'
+tmux "send-keys -t skelaplex:2 \"cd #{APP_DIR}; git show --stat\" C-m"
 
 need_chrome = `pgrep chrome`.empty?
 tmux 'new-session -d -s Browser -n Chrome'
@@ -55,7 +58,9 @@ end
 
 if `pgrep rubymine`.empty?
   tmux 'new-session -d -s IDE -n Rubymine'
-  tmux "send-keys -t IDE:0 \"#{IDE_PATH}\" C-m"
+  tmux "send-keys -t IDE:0 \"#{IDE_PATH} & disown\" C-m"
+  sleep 1
+  tmux 'kill-session -t IDE'
 end
 
 tmux 'select-window -t skelaplex:0'
